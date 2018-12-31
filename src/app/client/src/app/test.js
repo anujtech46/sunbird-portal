5cf03ba72e748b55baec904df63dec656ae52aee
anuj <anujtech46>
2018-12-31 12:32:13 +0530

    Issue chore: Update docker version image build number

---
+++ src/app/client/src/app/test.js
@@ -0,0 +1,20 @@
+import * as Rx from "rxjs";
+
+const subject = new Rx.BehaviorSubject();
+
+// subscriber 1
+subject.subscribe((data) => {
+    console.log('Subscriber A:', data);
+});
+
+subject.next(Math.random());
+subject.next(Math.random());
+
+// subscriber 2
+subject.subscribe((data) => {
+    console.log('Subscriber B:', data);
+});
+
+subject.next(Math.random());
+
+console.log(subject.value)
